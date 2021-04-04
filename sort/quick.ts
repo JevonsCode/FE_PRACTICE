9e0939d6de5c7a90f2221963eab742e33f2cf6f4
Jevons <36203025+JevonsCode@users.noreply.github.com>
2021-04-04 16:59:55 +0800

    feat(update): quick sort

--- sort/quick.ts
+++ sort/quick.ts
@@ -1,2 +1,23 @@
 const arr = [40, 33, 99, 23, 11, 45, 66, 86, 1024, -100, 0];
 
+function quick_sort(arr) {
+
+  if (arr.length <= 1) return arr;
+
+  //  中间这个选中的锚点就不用放在下面排序了
+  const anchor = arr.splice(~~(arr.length / 2), 1)[0];
+  
+  const left = [];
+  
+  const right = [];
+  
+  for(let i = 0; i < arr.length; i++) {
+    if (arr[i] > anchor) {
+      right.push(arr[i]);
+    } else {
+      left.push(arr[i]);
+    }
+  }
+
+  return [...quick_sort(left), anchor, ...quick_sort(right)];
+}
